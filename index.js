7039018a3ebf43897d5fa838fcb797d8e3fcd745
Priscila Aline Correa Batista <133381615+PriihA@users.noreply.github.com>
2024-08-28 10:48:19 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,8 @@
+const primeironome = 'Priscila';
+const idade ="15"
+const altura = "1.60"
+const peso = "50"
+const imc = peso / (altura * altura);
+
+
+console.log(`Paciente ${primeironome} tem idade ${idade} altura ${altura} massa ${peso} e seu imc é ${imc}`);
